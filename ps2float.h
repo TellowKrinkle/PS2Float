f878b525bba7dbcba43063d292cc08445aba5592
TellowKrinkle <tellowkrinkle@gmail.com>
2025-04-07 19:36:09 -0500

    Add ps2mul_avx

--- ps2float.h
+++ ps2float.h
@@ -30,6 +30,7 @@ __m128i ps2mul_one_avx2(__m128i a, __m128i b);
 __m128i ps2mul_one_avx(__m128i a, __m128i b);
 __m128i ps2mul_one_sse4(__m128i a, __m128i b);
 __m128i ps2mul_avx2(__m128i a, __m128i b);
+__m128i ps2mul_avx(__m128i a, __m128i b);
 #endif
 
 #ifdef __cplusplus
